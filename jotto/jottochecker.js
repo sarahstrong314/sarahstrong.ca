15c9287c5cfd8562bbe4d1478db577b1c2225adb
Sarah Strong <sarahstrong314@users.noreply.github.com>
2017-12-11 12:15:09 -0500

    Update jottochecker.js

--- jotto/jottochecker.js
+++ jotto/jottochecker.js
@@ -1,13 +1,13 @@
-var results = ['Result1', 'Result2', 'Result3', 'Result4', 'Result5', 'Result6', 'Result7', 'Result8', 'Result9', ]
+var results = ['Result1', 'Result2', 'Result3', 'Result4', 'Result5', 'Result6', 'Result7', 'Result8', 'Result9'];
     
 function checkWords() {
   count = 0;
-  word1 = word1.value
-  word2 = word2.value
+  word1 = word0.value
+  word2 = word1.value
     for (var i = 0; i < word1.length; i++) {
       if (word1[i] in word2) {
         count++;
         word2.replace(word1[i], '');
      }
-  translation.innerHTML = str(word1) + ' and 'str(word2) + ' have ' + str(count) + ' letters in common.'
+  Result1.innerHTML = String(word1) + ' and 'String(word2) + ' have ' + Str(count) + ' letters in common.'
 }
