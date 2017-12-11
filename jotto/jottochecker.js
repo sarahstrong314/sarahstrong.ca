4736b6cf6f100de0c32d172b66ac69426a18bf41
Sarah Strong <sarahstrong314@users.noreply.github.com>
2017-12-11 16:57:43 -0500

    Update jottochecker.js

--- jotto/jottochecker.js
+++ jotto/jottochecker.js
@@ -2,8 +2,8 @@ var results = ['Result1', 'Result2', 'Result3', 'Result4', 'Result5', 'Result6',
 
 function checkWords() {
   count = 0;
-  word1 = word0.value;
-  word2 = word1.value;
+  word1 = document.getElementById("word0").value;
+  word2 = document.getElementById("word1").value;
     for (var i = 0; i < word1.length; i++) {
       if (word1[i] in word2) {
         count++;
