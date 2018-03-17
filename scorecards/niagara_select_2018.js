84da1d4443ad00a1ae32c69ed83c13837f517495
Sarah Strong <sarahstrong314@users.noreply.github.com>
2018-03-17 16:54:29 -0400

    Update niagara_select_2018.js

--- scorecards/niagara_select_2018.js
+++ scorecards/niagara_select_2018.js
@@ -3,6 +3,6 @@ window.mo3events = ['6x6x6 Cube', '7x7x7 Cube', '3x3x3 Blindfolded', '4x4x4 Blin
 window.competitors = [
 	{ name: 'FirstName LastName', id: 'New Competitor', event: '3x3x3 One-Handed', round: 'Round 1', group: 5, cutoff: '45 seconds'},
   { name: 'FirstName LastName', id: 'New Competitor', event: 'Pyraminx', round: 'Round 1', group: 12, cutoff: ''},
-  { name: 'FirstName LastName',, id: '2018BLAH01', event: '7x7x7 Cube', round: 'Final', group: 2, cutoff: '10 minutes'},
-  { name: 'FirstName LastName',, id: '2018BLAH01', event: '3x3x3 Blindfolded', round: 'Combined Final', group: 3, cutoff: ''}
+  { name: 'FirstName LastName', id: '2018BLAH01', event: '7x7x7 Cube', round: 'Final', group: 2, cutoff: '10 minutes'},
+  { name: 'FirstName LastName', id: '2018BLAH01', event: '3x3x3 Blindfolded', round: 'Combined Final', group: 3, cutoff: ''}
 ];
