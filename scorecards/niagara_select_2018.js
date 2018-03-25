7a11ff962eefce474d723f0298da0ada311788a0
Sarah Strong <sarahstrong314@users.noreply.github.com>
2018-03-25 13:50:06 -0400

    Update niagara_select_2018.js

--- scorecards/niagara_select_2018.js
+++ scorecards/niagara_select_2018.js
@@ -1,8 +1,8 @@
 window.mo3events = [ '6x6x6 Cube', '7x7x7 Cube', '3x3x3 Blindfolded', '4x4x4 Blindfolded', '5x5x5 Blindfolded', '3x3x3 Multiple Blindfolded' ];
 
 window.competitors = [
-  { name: 'FirstName LastName', id: 'New Competitor', event: '3x3x3 One-Handed', round: 'Round 1', group: 5, cutoff: '45 seconds', limit: '10:00' },
-  { name: 'FirstName LastName', id: '2018BLAH01', event: 'Pyraminx', round: 'Round 1', group: 12, cutoff: '', limit: '10:00'},
-  { name: 'FirstName LastName', id: 'New Competitor', event: '7x7x7 Cube', round: 'Combined Final', group: 2, cutoff: '8 minutes', limit: '10:00'},
-  { name: 'FirstName LastName', id: '2018BLAH01', event: '3x3x3 Blindfolded', round: 'Final', group: 3, cutoff: '', limit: '10:00'}
+  { name: 'FirstName LastName', id: 'New Competitor', event: '3x3x3 One-Handed', round: 'Round 1 of 3', group: '5 of 12', cutoff: '45 seconds', limit: '10:00' },
+  { name: 'FirstName LastName', id: '2018BLAH01', event: 'Pyraminx', round: 'Round 1 of 2', group: '10 of 12', cutoff: '', limit: '10:00'},
+  { name: 'FirstName LastName', id: 'New Competitor', event: '7x7x7 Cube', round: 'Combined Final', group: '2 of 2', cutoff: '8 minutes', limit: '10:00'},
+  { name: 'FirstName LastName', id: '2018BLAH01', event: '3x3x3 Blindfolded', round: 'Final', group: '3 of 4', cutoff: '', limit: '10:00'}
 ];
