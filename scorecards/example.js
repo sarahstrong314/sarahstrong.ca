482cccf1d4401011dfddcb4bf45b625d1721c6c5
Sarah Strong <sarahstrong314@users.noreply.github.com>
2018-05-28 10:57:53 -0400

    Create example.js

---
+++ scorecards/example.js
@@ -0,0 +1,9 @@
+window.mo3events = [ '6x6x6 Cube', '7x7x7 Cube', '3x3x3 Blindfolded', '4x4x4 Blindfolded', '5x5x5 Blindfolded', '3x3x3 Multiple Blindfolded' ];
+
+window.competitors = [
+  { name: 'FirstName LastName', id: 'New Competitor', event: '5x5x5 Cube', round: 'Combined Final', group: '2 of 2', cutoff: '3:00.00', limit: '5:00'},
+  { name: 'FirstName LastName', id: 'New Competitor', event: '5x5x5 Cube', round: 'Combined Final', group: '2 of 2', cutoff: '3:00.00', limit: '5:00'},
+  { name: 'FirstName LastName', id: 'New Competitor', event: '5x5x5 Cube', round: 'Combined Final', group: '2 of 2', cutoff: '3:00.00', limit: '5:00'},
+  { name: 'FirstName LastName', id: 'New Competitor', event: '5x5x5 Cube', round: 'Combined Final', group: '2 of 2', cutoff: '3:00.00', limit: '5:00'}
+  ]
+  
