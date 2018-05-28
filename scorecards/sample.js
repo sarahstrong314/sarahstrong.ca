013dd354d417e58e826972df990a674cd80e929a
Sarah Strong <sarahstrong314@users.noreply.github.com>
2018-05-28 10:51:14 -0400

    Update sample.js

--- scorecards/sample.js
+++ scorecards/sample.js
@@ -3,6 +3,6 @@ window.mo3events = [ '6x6x6 Cube', '7x7x7 Cube', '3x3x3 Blindfolded', '4x4x4 Bli
 window.competitors = [
   { name: 'FirstName LastName', id: 'New Competitor', event: '3x3x3 One-Handed', round: 'Round 1 of 3', group: '5 of 12', cutoff: '45.00', limit: '10:00' },
   { name: 'FirstName LastName', id: '2018BLAH01', event: 'Pyraminx', round: 'Round 1 of 2', group: '10 of 12', cutoff: '', limit: '10:00'},
-  { name: 'FirstName LastName', id: 'New Competitor', event: '7x7x7 Cube', round: 'Combined Final', group: '2 of 2', cutoff: '8:00.00', limit: '10:00'},
+  { name: 'FirstName LastName', id: 'New Competitor', event: '5x5x5 Cube', round: 'Combined Final', group: '2 of 2', cutoff: '3:00.00', limit: '5:00'},
   { name: 'FirstName LastName', id: '2018BLAH01', event: '3x3x3 Blindfolded', round: 'Final', group: '3 of 4', cutoff: '', limit: '10:00'}
 ];
