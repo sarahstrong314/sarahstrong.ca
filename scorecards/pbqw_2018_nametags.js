ea0c175b5fe3a74ba97c89a46e1757c9e037e533
Sarah Strong <sarahstrong314@users.noreply.github.com>
2018-06-01 13:18:51 -0400

    Create pbqw_2018_nametags.js

---
+++ scorecards/pbqw_2018_nametags.js
@@ -0,0 +1,23 @@
+window.competitors = [
+{ name: 'Alyssa Esparaz', id: '2014ESPA01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Ben Bergen', id: '2015BERG10', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Ben Madison', id: '2016MADI02', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Bill Wang', id: '2010WANG68', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Brady Metherall', id: '2009METH01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Colleen Geerlinks', id: '2014GEER01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Craig Bouchard', id: '2005BOUC01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Derek Kong', id: '2017KONG08', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Emile Compion', id: '2007COMP01', country: 'South Africa', title: 'COMPETITOR' },
+{ name: 'James Hocevar', id: '2016HOCE02', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Jonathan Esparaz', id: '2013ESPA01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Julian David', id: '2010DAVI06', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Marco Yang', id: '2017YANG62', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Max Xiong', id: '2015XION03', country: 'China', title: 'COMPETITOR' },
+{ name: 'Nicholas Patterson', id: '2016PATT02', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Paul Ellement', id: '2016ELLE01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Ray Vince Ong', id: '2012ONGR01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Richard Madison', id: '2017MADI03', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Ryan DeLine', id: '2012DELI01', country: 'USA', title: 'COMPETITOR' },
+{ name: 'Sarah Strong', id: '2007STRO01', country: 'Canada', title: 'ORGANIZER' },
+{ name: 'Taha Ben Salah', id: '2015SALA03', country: 'Tunisia', title: 'COMPETITOR' }
+]
