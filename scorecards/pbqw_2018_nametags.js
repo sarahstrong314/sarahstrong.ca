416add64f6de68f7ee87cd81f456844ea7ff100d
Sarah Strong <sarahstrong314@users.noreply.github.com>
2018-06-01 18:25:37 -0400

    Update pbqw_2018_nametags.js

--- scorecards/pbqw_2018_nametags.js
+++ scorecards/pbqw_2018_nametags.js
@@ -4,7 +4,7 @@ window.competitors = [
 { name: 'Ben Madison', id: '2016MADI02', country: 'Canada', title: 'COMPETITOR' },
 { name: 'Bill Wang', id: '2010WANG68', country: 'Canada', title: 'COMPETITOR' },
 { name: 'Brady Metherall', id: '2009METH01', country: 'Canada', title: 'COMPETITOR' },
-{ name: 'Colleen Geerlinks', id: '2014GEER01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Colleen Geerlinks', id: '2014GEER01', country: 'Canada', title: 'VOLUNTEER' },
 { name: 'Craig Bouchard', id: '2005BOUC01', country: 'Canada', title: 'COMPETITOR' },
 { name: 'Derek Kong', id: '2017KONG08', country: 'Canada', title: 'COMPETITOR' },
 { name: 'Emile Compion', id: '2007COMP01', country: 'South Africa', title: 'COMPETITOR' },
