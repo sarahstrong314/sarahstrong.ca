1814a163599d0b21b867aeabfd413e43b70ab439
Sarah Strong <sarahstrong314@users.noreply.github.com>
2018-06-05 16:50:10 -0400

    Update n3w_2018_scorecards.js

--- scorecards/n3w_2018_scorecards.js
+++ scorecards/n3w_2018_scorecards.js
@@ -1,4 +1,6 @@
-window.mo3events = [ '6x6x6 Cube', '7x7x7 Cube', '3x3x3 Blindfolded', '4x4x4 Blindfolded', '5x5x5 Blindfolded', '3x3x3 Multiple Blindfolded' ];
+window.mo3events =[ '6x6x6 Cube', '7x7x7 Cube', '3x3x3 Blindfolded', '4x4x4 Blindfolded', '5x5x5 Blindfolded'];
+
+window.bo1events = ['3x3x3 Multiple Blindfolded'];
 
 window.competitors = [
 { name: 'Alex Keats', id: '2012KEAT01', event: '3x3x3 With Feet', round: 'Combined Final', group: '1 of 2', cutoff: '3:30', limit: '5:00' },
