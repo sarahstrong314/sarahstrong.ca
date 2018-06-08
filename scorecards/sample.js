3103ef2ab2d6597b9594e576faf710225640ab97
Sarah Strong <sa967st@gmail.com>
2018-06-08 17:40:26 -0400

    Reorganized competition stuff

--- scorecards/sample.js
+++ scorecards/sample.js
@@ -3,9 +3,9 @@ window.mo3events =[ '6x6x6 Cube', '7x7x7 Cube', '3x3x3 Blindfolded', '4x4x4 Blin
 window.bo1events = ['3x3x3 Multiple Blindfolded']
 
 window.competitors = [
-  { name: 'FirstName LastName', id: 'New Competitor', event: '3x3x3 One-Handed', round: 'Round 1 of 3', group: '5 of 12', cutoff: '45.00', limit: '10:00' },
-  { name: 'FirstName LastName', id: '2018BLAH01', event: 'Pyraminx', round: 'Round 1 of 2', group: '10 of 12', cutoff: '', limit: '10:00'},
+  { name: 'FirstName LastName', id: 'New Competitor', event: '4x4x4 Cube', round: 'Round 1 of 3', group: '3 of 4', cutoff: '2:00', limit: '5:00' },
+  { name: 'FirstName LastName', id: '2018BLAH01', event: 'Skewb', round: 'Round 1 of 2', group: '5 of 8', cutoff: '', limit: '5:00'},
   { name: 'FirstName LastName', id: 'New Competitor', event: '7x7x7 Cube', round: 'Combined Final', group: '2 of 2', cutoff: '8:00', limit: '10:00'},
-  { name: 'FirstName LastName', id: '2018BLAH01', event: '3x3x3 Blindfolded', round: 'Final', group: '3 of 4', cutoff: '', limit: '10:00'},
+  { name: 'FirstName LastName', id: '2018BLAH01', event: '3x3x3 Blindfolded', round: 'Final', group: '1 of 2', cutoff: '', limit: '5:00'},
   { name: 'FirstName LastName', id: 'New Competitor', event: '3x3x3 Multiple Blindfolded', round: 'Combined Final', group: '1 of 1', cutoff: '', limit: '60:00'}
 ];
