3ece22bd0ff97415f4ce8f2d04b205e06f51da10
Sarah Strong <sarahstrong314@users.noreply.github.com>
2018-09-07 19:37:46 -0400

    Update sample.js

--- scorecards/sample.js
+++ scorecards/sample.js
@@ -7,5 +7,5 @@ window.competitors = [
   { name: 'FirstName LastName', id: '2018BLAH01', event: 'Skewb', round: 'Round 1 of 2', group: '5 of 8', cutoff: '', limit: '5:00'},
   { name: 'FirstName LastName', id: 'New Competitor', event: '7x7x7 Cube', round: 'Combined Final', group: '2 of 2', cutoff: '8:00', limit: '10:00'},
   { name: 'FirstName LastName', id: '2018BLAH01', event: '3x3x3 Blindfolded', round: 'Final', group: '1 of 2', cutoff: '', limit: '5:00'},
-  { name: 'FirstName LastName', id: 'New Competitor', event: '3x3x3 Multiple Blindfolded', round: 'Combined Final', group: '1 of 1', cutoff: '', limit: '60:00'}
+  { name: 'FirstName LastName', id: 'New Competitor', event: '3x3x3 Multi-Blind', round: 'Combined Final', group: '1 of 1', cutoff: '', limit: '60:00'}
 ];
