6dff77392b4653ae1038b8d87caf10768f7181dd
Sarah Strong <sarahstrong314@users.noreply.github.com>
2019-01-06 10:54:26 -0500

    Update sample.js

--- scorecards/sample.js
+++ scorecards/sample.js
@@ -1,6 +1,6 @@
 window.mo3events =[ '6x6x6 Cube', '7x7x7 Cube', '3x3x3 Blindfolded', '4x4x4 Blindfolded', '5x5x5 Blindfolded'];
 
-window.bo1events = ['3x3x3 Multiple Blindfolded']
+window.bo1events = ['3x3x3 Multi-Blind']
 
 window.competitors = [
   { name: 'FirstName LastName', id: 'New Competitor', event: '4x4x4 Cube', round: 'Round 1 of 3', group: '3 of 4', cutoff: '2:00', limit: '5:00' },
