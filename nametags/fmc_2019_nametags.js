f049f9b6ba1510fba113d6f1ef3e0a1deb8078f2
Sarah Strong <sarahstrong314@users.noreply.github.com>
2019-06-13 08:24:24 -0400

    Add files via upload

---
+++ nametags/fmc_2019_nametags.js
@@ -0,0 +1,34 @@
+window.size58 = []
+window.size56 = []
+window.size54 = []
+window.size52 = []
+window.size50 = [] 
+window.size48 = ['Sebastian Desaulniers'] 
+window.size46 = [] 
+window.size44 = [] 
+window.size42 = []
+
+window.competitors = [
+{ name: 'Abdullah Gulab', id: '2014GULA02', country: 'Canada', title: 'ORGANIZER' },
+{ name: 'Aiden Chan', id: '2018CHAN31', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Andrew Nguyen', id: '2011NGUY03', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Ben Bergen', id: '2015BERG10', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Ben Rockman', id: '2014ROCK01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Benson Lau', id: '2019LAUB01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Brady Metherall', id: '2009METH01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'David Drobner', id: '2018DROB01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Joaquin Abarca', id: '2017ABAR02', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Jonathan Esparaz', id: '2013ESPA01', country: 'Canada', title: 'ORGANIZER' },
+{ name: 'Kevin Tang', id: '2017TANG21', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Max Murphy', id: '2016MURP05', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Max Sun', id: '2017SUNM02', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Nima Bolourchian', id: '2017BOLO01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Owen Widdis', id: '2015WIDD01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Ray Vince Ong', id: '2012ONGR01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Roan Paone', id: '2017PAON01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Ryan Pin Harry', id: '2015HARR01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Ryan Wu', id: '2017WURY01', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Sarah Strong', id: '2007STRO01', country: 'Canada', title: 'DELEGATE' },
+{ name: 'Sebastian Desaulniers', id: '2018DESA05', country: 'Canada', title: 'COMPETITOR' },
+{ name: 'Thomas Imada', id: '2018IMAD01', country: 'Canada', title: 'COMPETITOR' },
+]
